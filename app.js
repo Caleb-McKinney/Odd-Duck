198461fa7be9f16a96d1927b3798e8b03600fa79
Caleb McKinney <McKinney.Caleb@gmail.com>
2023-09-14 20:44:58 -0500

    got images to render and views counting

--- app.js
+++ app.js
@@ -2,32 +2,32 @@
 
 const state = {
   ducks: [
-    new Duck("R2D2 Bag", "images/bag.jpg", "R2D2 Bag"),
-    new Duck("Banana Cutter", "images/banana.jpg"),
-    new Duck("iBathroom", "images/bathroom.jpg"),
-    new Duck("Rain Boots", "images/boots.jpg"),
-    new Duck("Breakfast Machine", "images/breakfast.jpg"),
-    new Duck("Bubble Balls", "images/bubblegum.jpg"),
-    new Duck("Red Chair", "images/chair.jpg"),
-    new Duck("Cruisin-Goat", "images/cruisin-goat.jpg"),
-    new Duck("Cthulhu", "images/cthulhu.jpg"),
-    new Duck("Dog-Duck", "images/dog-duck.jpg"),
-    new Duck("Dragon Meat", "images/dragon.jpg"),
-    new Duck("Pen Cuttlery", "images/pen.jpg"),
-    new Duck("Pet-Sweep", "images/pet-sweep.jpg"),
-    new Duck("Pizza Scissors", "images/scissors.jpg"),
-    new Duck("Shark Sleeper", "images/shark.jpg"),
-    new Duck("Sweeper", "images/sweep.png"),
-    new Duck("Tauntaun", "images/tauntaun.jpg"),
-    new Duck("Unicorn", "images/unicorn.jpg"),
-    new Duck("water-can", "images/water-can.jpg"),
-    new Duck("wine-glass", "images/wine-glass.jpg"),
+    new Duck("R2D2 Bag", "./images/bag.jpg"),
+    new Duck("Banana Cutter", "./images/banana.jpg"),
+    new Duck("iBathroom", "./images/bathroom.jpg"),
+    new Duck("Rain Boots", "./images/boots.jpg"),
+    new Duck("Breakfast Machine", "./images/breakfast.jpg"),
+    new Duck("Bubble Balls", "./images/bubblegum.jpg"),
+    new Duck("Red Chair", "./images/chair.jpg"),
+    new Duck("Cruisin-Goat", "./images/cruisin-goat.jpg"),
+    new Duck("Cthulhu", "./images/cthulhu.jpg"),
+    new Duck("Dog-Duck", "./images/dog-duck.jpg"),
+    new Duck("Dragon Meat", "./images/dragon.jpg"),
+    new Duck("Pen Cuttlery", "./images/pen.jpg"),
+    new Duck("Pet-Sweep", "./images/pet-sweep.jpg"),
+    new Duck("Pizza Scissors", "./images/scissors.jpg"),
+    new Duck("Shark Sleeper", "./images/shark.jpg"),
+    new Duck("Sweeper", "./images/sweep.png"),
+    new Duck("Tauntaun", "./images/tauntaun.jpg"),
+    new Duck("Unicorn", "./images/unicorn.jpg"),
+    new Duck("water-can", "./images/water-can.jpg"),
+    new Duck("wine-glass", "./images/wine-glass.jpg"),
   ],
   imageContainer: document.getElementById("image-container"),
   resultsSection: document.getElementById("results"),
   duckChart: null,
 };
-
+console.log()
 // constructor function for ducks
 
 function Duck(name, filePath) {
@@ -39,28 +39,38 @@ this.views = 0;
 }
 //method to render duck image
 Duck.prototype.render = function() {
-  const duckImg = document.createElement("img");
-  duckImg.src = this.filePath;
-  duckImg.alt = this.name;
-
-  state.imageContainer.appendChild(duckImg);
+  const duckImgTag = document.createElement("img");
+  duckImgTag.setAttribute("src", this.filepath);
+  duckImgTag.setAttribute("alt", this.name);
+  
+console.log("duckImg", duckImgTag)
+  state.imageContainer.appendChild(duckImgTag);
 };
 
 function renderDucks() {
   state.imageContainer.innerHTML = "";
   state.imageContainer.addEventListener("click", handleClickDuck);
   let duckOne = state.ducks[getRandomInt(0, state.ducks.length)];
-  let duckTwo = state.ducks[(0, state.ducks.length)];
+  let duckTwo = state.ducks[getRandomInt(0, state.ducks.length)];
+  let duckThree = state.ducks[getRandomInt(0, state.ducks.length)];
 
   while (duckOne === duckTwo) {
      duckOne = state.ducks[getRandomInt(0, state.ducks.length)];
   }
+  while (duckTwo === duckThree) {
+     duckOne = state.ducks[getRandomInt(0, state.ducks.length)];
+  }
+  while (duckOne === duckThree) {
+     duckOne = state.ducks[getRandomInt(0, state.ducks.length)];
+  }
 
   duckOne.views++;
-  // duckTwo.views++;
+  duckTwo.views++;
+  duckThree.views++;
 
 duckOne.render();
-// duckTwo.render();
+duckTwo.render();
+duckThree.render();
 }
 
 renderDucks();
@@ -68,11 +78,11 @@ renderDucks();
 function handleClickDuck(event) {
   event.preventDefault();
 
-  const target = event.target.alt;
+  const target = event.target.src;
 
   //loop through the ducks array, once we found one that was clicked, we increment the duck objects clicks
   for (let i = 0; i < state.ducks.length; i++) {
-    if (target === state.ducks[i].name) {
+    if (target === state.ducks[i].filepath) {
       state.ducks[i].votes++;
     }
   }
